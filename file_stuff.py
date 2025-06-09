9ef6ef4d7ba393fd35593489a3d78739a1182778
Gresha Shah <greshashah601@gmail.com>
2025-06-09 20:38:52 +0200

    Initial commit

--- file_stuff.py
+++ file_stuff.py
@@ -1,22 +1,26 @@
-# file_stuff.py
-
 class SimpleReader:
     def __init__(self, filename):
-        self.filename = filename
+        self._filename = filename  # "Private" style
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def line_count(self):
+        return sum(1 for _ in open(self._filename))
 
     def read_lines(self):
-        # Generator that yields each line without newline
-        with open(self.filename, 'r') as f:
+        with open(self._filename, 'r') as f:
             for line in f:
                 yield line.strip()
 
     def greet(self):
-        # Simple method to test with pytest
         return "Hi from SimpleReader!"
+
 
 class FileCombiner(SimpleReader):
     def __add__(self, other):
-        # Use + to combine two files into combined.txt
         output = "combined.txt"
         with open(self.filename, 'r') as f1, open(other.filename, 'r') as f2, open(output, 'w') as out:
             out.write(f1.read())
@@ -25,10 +29,8 @@ class FileCombiner(SimpleReader):
         return FileCombiner(output)
 
     def combine_multiple(self, *others):
-        # Append multiple files to this one
         with open(self.filename, 'a') as out:
             for other in others:
                 with open(other.filename, 'r') as f:
                     out.write("\n")
                     out.write(f.read())
-
